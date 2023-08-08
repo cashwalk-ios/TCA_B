43cbf340616a8a3e385319451424dcb229eff3bf
alim <alims@cashwalk.io>
2023-08-08 15:16:49 +0900

    start

---
+++ TCA_B/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  TCA_B
+//
+//  Created by 이아림 on 2023/08/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, TCA_B 🤟")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TCA_B/TCA_BApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_BApp.swift
+//  TCA_B
+//
+//  Created by 이아림 on 2023/08/08.
+//
+
+import SwiftUI
+
+@main
+struct TCA_BApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
